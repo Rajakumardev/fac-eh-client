455231ea8f670b01471a797e56f6efa373864eed
Rajakumardev <rajakumarofficial@gmail.com>
2020-08-01 17:58:51 +0530

    RoomForm added

---
+++ src/component/RoomForm/RoomForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import heroImage from '../../assets/images/hero-image.svg';
+
+function RoomForm() {
+    return (
+        <div className="mx-4 p-4 font-sans text-gray-800 font-bold rounded-lg flex items-center justify-center">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2">
+            <div className="mb-8 border-l-4 border-gray-700 p-2 px-5 text-gray-700">
+                    <blockquote><span className="py-1 px-4 rounded-lg bg-gray-500 text-white">join</span> existing room or <span className="py-1 px-4 rounded-lg bg-gray-500 text-white">create</span> new room</blockquote>
+                </div>
+                <div className="mb-4 flex justify-between">
+                    <input className="shadow appearance-none border rounded-l w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="invitelink" type="text" placeholder="Invite link"/>
+                    <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-r focus:outline-none focus:shadow-outline" type="button">
+                        Join
+                    </button>
+                </div>
+                
+                <div className="flex items-center justify-around">
+                    <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        Create new room
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default RoomForm
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Header from '../component/Header/Header'
+import RoomForm from '../component/RoomForm/RoomForm'
 
 function Home() {
     return (
-        <div>
+        <Fragment>
             <Header/>
-        </div>
+            <RoomForm/>
+        </Fragment>
     )
 }
 
