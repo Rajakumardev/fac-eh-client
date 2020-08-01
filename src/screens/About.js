4d55c23b09a7344dc8ce882f053c21dde5f19a59
Rajakumardev <rajakumarofficial@gmail.com>
2020-08-01 18:34:02 +0530

    About page added

--- src/screens/About.js
+++ src/screens/About.js
@@ -3,7 +3,15 @@ import React, { Fragment } from 'react'
 function About() {
     return (
         <Fragment>
-            <h1>About</h1>
+            <div className="mx-4 p-4 font-sans text-gray-800 font-bold rounded-lg flex flex-col justify-start bg-white">
+                <h2><span className="py-1 px-4 rounded-lg bg-gray-500 text-white">About</span></h2>
+                <div className="w-full my-4">
+                    <p>
+                        Thanks for using fac-eh!
+                        {/* later */}
+                    </p>
+                </div>
+            </div>
         </Fragment>
     )
 }
